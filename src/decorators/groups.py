44f24a13379a58a90f0225d07564bb6fcb1dc978
baronsmv <baronsmv@gmail.com>
2024-11-17 16:26:25 -0600

    Added docstrings and subcommands to CLI. Improved modularity of decorators.

--- src/decorators/groups.py
+++ src/decorators/groups.py
@@ -5,29 +5,30 @@ from click_help_colors import HelpColorsGroup
 
 from src.defaults import defaults
 
-
-@click.group(
+main_group = click.group(
     cls=HelpColorsGroup,
     help_headers_color=defaults.HEADERS_COLOR,
     help_options_color=defaults.OPTIONS_COLOR,
 )
+
+
+@main_group
 def cli() -> None:
     pass
 
 
-@cli.group(
+sub_group = cli.group(
     cls=HelpColorsGroup,
     help_headers_color=defaults.HEADERS_COLOR,
     help_options_color=defaults.OPTIONS_COLOR,
 )
+
+
+@sub_group
 def download() -> None:
     pass
 
 
-@cli.group(
-    cls=HelpColorsGroup,
-    help_headers_color=defaults.HEADERS_COLOR,
-    help_options_color=defaults.OPTIONS_COLOR,
-)
+@sub_group
 def identify() -> None:
     pass
