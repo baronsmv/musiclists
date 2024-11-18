884fe6458addd6b12347c1016f53031ccbf45840
baronsmv <baronsmv@gmail.com>
2024-11-17 19:33:16 -0600

    Added docstring to dedup.dedup, and added only_highest_match parameter.

--- src/copy.py
+++ src/copy.py
@@ -19,24 +19,24 @@ def copy(
     Recursively copy directories listed in a JSON file from one directory to
     another.
 
-    The primary use case is to copy selected albums (from the ``wanted`` list)
+    The primary use case is to copy selected albums (from the `wanted` list)
     from a large music library to a more curated or selective library.
 
     Parameters
     ----------
-        data:
-            Path to the JSON file, which contains a dictionary of album
-            objects, each with at least the fields: artist, title, and year.
-        source:
-            Path to the source directory containing the albums.
-        destination:
-            Path to the target directory where albums will be copied.
-        verbose:
-            Show detailed information about the copy process, such as the
-            directories being copied.
-        debug:
-            Enable debug-level logging for troubleshooting, showing additional
-            information for debugging purposes.
+    data:
+        Path to the JSON file, which contains a dictionary of album
+        objects, each with at least the fields: artist, title, and year.
+    source:
+        Path to the source directory containing the albums.
+    destination:
+        Path to the target directory where albums will be copied.
+    verbose:
+        Show detailed information about the copy process, such as the
+        directories being copied.
+    debug:
+        Enable debug-level logging for troubleshooting, showing additional
+        information for debugging purposes.
     """
     if verbose:
         print(f"""
@@ -45,7 +45,7 @@ def copy(
               Destination: {destination}
               """)
     for d in load(data):
-        path = get.path(d)
+        path = get.path(d)  # Formatting `d` to its string path equivalent.
         if verbose:
             print(f"Moving {path}.")
         fromPath = Path(source / path)
--- src/dedup.py
+++ src/dedup.py
@@ -10,18 +10,70 @@ from src import get
 def dedup(
     data1: dict,
     data2: dict,
-    lowerlimit: int | float = 0.6,
-    upperlimit: int | float = 1,
+    minimum: int | float = 0.6,
+    maximum: int | float = 1,
+    only_highest_match: bool = defaults.ONLY_HIGHEST_MATCH,
     verbose: bool = defaults.VERBOSE,
     debug: bool = defaults.VERBOSE,
 ) -> Iterator[dict[str, str]]:
     """
-    Compare each of the objects
+    Compare each object (album) in two dictionaries containing music album
+    data from two JSON files and returns a generator of matching album entries
+    where the similarity score falls within a specified range.
+
+    The comparison is done using difflib.SequenceMatcher, which computes a
+    similarity ratio between pairs of album objects. Only albums with a
+    similarity score within the specified range (inclusive on the low end,
+    exclusive on the high end) are yielded.
+
+    Parameters
+    ----------
+    data1:
+        A dictionary of music album objects from the first JSON file.
+    data2:
+        A dictionary of music album objects from the second JSON file.
+    minimum:
+        The minimum similarity score (inclusive).
+    maximum:
+        The maximum similarity score (exclusive).
+    only_highest_match:
+        If True, only the highest similarity match for each album in `data1`
+        will be yielded. If False, all matching albums within the similarity
+        range will be returned.
+    verbose:
+        If True, prints additional information during execution.
+    debug:
+        If True, prints debug-level information.
+
+    Returns
+    -------
+    A generator yielding dictionaries of matching album entries where the
+    similarity score between the objects falls within the specified range.
+
+    Notes
+    -----
+    - The function uses difflib.SequenceMatcher to calculate the similarity
+      between the album entries. The similarity ratio ranges from 0 to 1, with
+      1 indicating identical objects.
+    - To fine-tune the matching process, adjust the `minimum` and
+      `maximum` values. For instance, a minimum value of 0.7 and an maximum
+      value of 0.9 will only match albums with a similarity score between 0.7
+      (inclusive) and 0.9 (exclusive).
+    - When `only_highest_match` is set to `True`, for each album in `data1`,
+      only the match from `data2` with the highest similarity score will be
+      returned. This can be useful when you want to minimize the number of
+      matches and focus on the best possible pairings.
     """
+    if only_highest_match:
+        highest_match = (float(), {str(): str()})
     for d1 in data1.values():
         for d2 in data2.values():
             a1 = get.path(d1)
             a2 = get.path(d2)
             sim = SequenceMatcher(None, a1, a2).ratio()
-            if lowerlimit <= sim < upperlimit:
-                yield {a1: a2}
+            if minimum <= sim < maximum:
+                if only_highest_match and sim > highest_match[0]:
+                    highest_match = (sim, {a1: a2})
+                else:
+                    yield {a1: a2}
+            yield highest_match[1]
