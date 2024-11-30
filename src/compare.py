1ac8e871b954db1fecb635f5487bc915d680144a
baronsmv <baronsmv@gmail.com>
2024-11-30 12:59:19 -0600

    Moved src.compare.diff() to src.get.df.similarities().

--- src/compare.py
+++ src/compare.py
@@ -1,65 +1,11 @@
 #!/usr/bin/env python3
 
-from difflib import SequenceMatcher
-from statistics import median
-
-from src import load
 from src.defaults import defaults
-from src.get.album import path
+
+from src.get import df
 
 
-def __diff__(
-    data_1: str,
-    data_2: str,
-    columns: list | tuple,
-    minimum: int | float,
-    maximum: int | float,
-    only_highest_match: bool,
-    num_diff: float,
-    quiet: bool = defaults.QUIET,
-    verbose: bool = defaults.VERBOSE,
-    debug: bool = defaults.DEBUG,
-):
-    if not quiet:
-        print(f"Finding similarities between {data_1} and {data_2}...")
-    highest_ratio = 0.0
-    highest_match = dict()
-    for d1 in load.df(data_1).rows(named=True):
-        for d2 in load.df(data_2).rows(named=True):
-            # print(tuple((d1[col], d2[col]) for col in columns))
-            # exit()
-            sim = median(
-                (
-                    SequenceMatcher(None, d1[col], d2[col]).ratio()
-                    if isinstance(d1[col], str) or isinstance(d2[col], str)
-                    else 1 - (abs(d1[col] - d2[col]) * num_diff)
-                )
-                for col in columns
-            )
-            if minimum <= sim < maximum:
-                if only_highest_match and sim > highest_ratio:
-                    highest_match = {d1: d2}
-                elif not only_highest_match:
-                    yield {d1: d2}
-                    if verbose:
-                        print(
-                            f"- Found similarity between {path(d1)} and {path(d2)}."
-                        )
-        if only_highest_match and highest_match:
-            yield highest_match
-            if verbose:
-                print(
-                    f"- Found the highest similarity between "
-                    + path(tuple(highest_match.items())[0])
-                    + " and "
-                    + path(tuple(highest_match.items())[1])
-                    + "."
-                )
-            highest_ratio = 0.0
-            highest_match.clear()
-
-
-def diff(
+def similarities(
     data_1: str,
     data_2: str,
     columns: list | tuple = ("title", "artist", "year"),
@@ -71,7 +17,7 @@ def diff(
     verbose: bool = defaults.VERBOSE,
     debug: bool = defaults.DEBUG,
 ):
-    for d in __diff__(
+    for s in df.similarities(
         data_1=data_1,
         data_2=data_2,
         columns=columns,
@@ -83,4 +29,4 @@ def diff(
         verbose=verbose,
         debug=debug,
     ):
-        print(d)
+        print(s)
--- src/get/df.py
+++ src/get/df.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python3
 
+from difflib import SequenceMatcher
+from statistics import median
+
 import polars as pl
+
+from src import load
+from src.get.album import path
 
 
 def contextualize(
@@ -19,7 +25,7 @@ def contextualize(
             for k, v in num_filter.items()
         )
     if sort_by:
-        df = df.sort(sort_by.keys(), descending=tuple(sort_by.values()))
+        df = df.sort(sort_by.keys(), descending=list(sort_by.values()))
     if select:
         if isinstance(select, dict):
             df = df.select(select.keys())
@@ -42,3 +48,54 @@ def tracks(df: pl.DataFrame) -> pl.DataFrame:
         how="horizontal",
     )
     return df
+
+
+def similarities(
+    data_1: str,
+    data_2: str,
+    columns: list | tuple,
+    minimum: int | float,
+    maximum: int | float,
+    only_highest_match: bool,
+    num_diff: float,
+    quiet: bool,
+    verbose: bool,
+    debug: bool,
+):
+    if not quiet:
+        print(f"Finding similarities between {data_1} and {data_2}...")
+    highest_ratio = 0.0
+    highest_match = dict()
+    for d1 in load.df(data_1).rows(named=True):
+        for d2 in load.df(data_2).rows(named=True):
+            # print(tuple((d1[col], d2[col]) for col in columns))
+            # exit()
+            match = median(
+                (
+                    SequenceMatcher(None, d1[col], d2[col]).ratio()
+                    if isinstance(d1[col], str) or isinstance(d2[col], str)
+                    else 1 - (abs(d1[col] - d2[col]) * num_diff)
+                )
+                for col in columns
+            )
+            if minimum <= match < maximum:
+                if only_highest_match and match > highest_ratio:
+                    highest_match = {d1: d2}
+                elif not only_highest_match:
+                    yield {d1: d2}
+                    if verbose:
+                        print(
+                            f"- Found similarity between {path(d1)} and {path(d2)}."
+                        )
+        if only_highest_match and highest_match:
+            yield highest_match
+            if verbose:
+                print(
+                    f"- Found the highest similarity between "
+                    + path(tuple(highest_match.items())[0])
+                    + " and "
+                    + path(tuple(highest_match.items())[1])
+                    + "."
+                )
+            highest_ratio = 0.0
+            highest_match.clear()
